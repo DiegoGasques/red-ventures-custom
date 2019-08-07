1bffd065ce50d3091911ebb0081a0fe6da41bcda
Diego <diegomgasques@gmail.com>
2019-08-07 12:40:13 -0300

    [Style] Add transition to CustomEngineItems

--- src/store/reducers/custom.reducer.js
+++ src/store/reducers/custom.reducer.js
@@ -1,7 +1,6 @@
 import { SET_CUSTOM, CHANGE_CUSTOM, CLEAR_CUSTOM } from "../action.types.js";
 
 export default (state = [], action) => {
-  console.log("REDUCING!!!");
   switch (action.type) {
     case SET_CUSTOM:
       return [...state, action.payload];
