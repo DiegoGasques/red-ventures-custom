01f553198a07dc1f1c46908b302127f504adf74d
Diego <diegomgasques@gmail.com>
2019-08-07 06:25:31 -0300

    [Styles] Media queries for final page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 
 export const NavBar = () => {
   return (
     <header className="NavBar">
-      <h1 className="logo">
-        <img src={logo} alt="Red Ventures" />
-      </h1>
+      <Link to="/">
+        <h1 className="logo">
+          <img src={logo} alt="Red Ventures" />
+        </h1>
+      </Link>
       <nav className="nav">
         <ul className="list">
           <li className="item active">Model R</li>
